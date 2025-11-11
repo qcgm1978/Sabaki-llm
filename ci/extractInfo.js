c79817df2d5dce0347a8d252b91dc8e702f2f3be
qcgm197874@gmail.com <qcgm197874@gmail.com>
2025-11-11 17:47:50 +0800

    Release v0.52.2

--- ci/extractInfo.js
+++ ci/extractInfo.js
@@ -1,10 +1,20 @@
 const os = require('os')
 const path = require('path')
+const fs = require('fs')
 const {version} = require('../package.json')
 
 function printSetOutputs(outputs) {
-  for (let [name, value] of Object.entries(outputs)) {
-    console.log(`::set-output name=${name}::${value}`)
+  const githubOutput = process.env.GITHUB_OUTPUT
+  if (githubOutput) {
+    // 使用新的环境文件方式
+    for (let [name, value] of Object.entries(outputs)) {
+      fs.appendFileSync(githubOutput, `${name}=${value}\n`)
+    }
+  } else {
+    // 兼容旧版本，本地运行时使用console输出
+    for (let [name, value] of Object.entries(outputs)) {
+      console.log(`::set-output name=${name}::${value}`)
+    }
   }
 }
 
